44ded751b1bdd7ede1b7ca8cbf5da52d87b95398
Tommaso Fonda <fondatommaso2@gmail.com>
2022-11-09 15:07:29 +0100

    Updater: Implement a simple beta switch
    Change-Id: I78ffc9c253d97006e242aa9f3dc1e3b5e2736c92

--- src/org/lineageos/updater/UpdatesActivity.java
+++ src/org/lineageos/updater/UpdatesActivity.java
@@ -484,6 +484,7 @@ public class UpdatesActivity extends UpdatesListActivity {
         SwitchCompat dataWarning = view.findViewById(R.id.preferences_mobile_data_warning);
         SwitchCompat abPerfMode = view.findViewById(R.id.preferences_ab_perf_mode);
         SwitchCompat updateRecovery = view.findViewById(R.id.preferences_update_recovery);
+        SwitchCompat betaChannel = view.findViewById(R.id.preferences_beta_channel);
 
         if (!Utils.isABDevice()) {
             abPerfMode.setVisibility(View.GONE);
@@ -494,6 +495,7 @@ public class UpdatesActivity extends UpdatesListActivity {
         autoDelete.setChecked(prefs.getBoolean(Constants.PREF_AUTO_DELETE_UPDATES, false));
         dataWarning.setChecked(prefs.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true));
         abPerfMode.setChecked(prefs.getBoolean(Constants.PREF_AB_PERF_MODE, false));
+        betaChannel.setChecked(prefs.getBoolean(Constants.PREF_BETA_CHANNEL, false));
 
         if (getResources().getBoolean(R.bool.config_hideRecoveryUpdate)) {
             // Hide the update feature if explicitly requested.
@@ -533,6 +535,7 @@ public class UpdatesActivity extends UpdatesListActivity {
                             .putBoolean(Constants.PREF_AUTO_DELETE_UPDATES, autoDelete.isChecked())
                             .putBoolean(Constants.PREF_MOBILE_DATA_WARNING, dataWarning.isChecked())
                             .putBoolean(Constants.PREF_AB_PERF_MODE, abPerfMode.isChecked())
+                            .putBoolean(Constants.PREF_BETA_CHANNEL, betaChannel.isChecked())
                             .apply();
 
                     if (Utils.isUpdateCheckEnabled(this)) {
