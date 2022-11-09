6f2712f19637942192efabaecb061b63eb11aac9
Tommaso Fonda <fondatommaso2@gmail.com>
2022-11-09 15:07:29 +0100

    Updater: Implement a simple beta switch
    Change-Id: I78ffc9c253d97006e242aa9f3dc1e3b5e2736c92

--- app/src/main/java/org/lineageos/updater/UpdatesActivity.java
+++ app/src/main/java/org/lineageos/updater/UpdatesActivity.java
@@ -506,6 +506,7 @@ public class UpdatesActivity extends UpdatesListActivity {
                 R.id.preferences_metered_network_warning);
         SwitchCompat abPerfMode = view.findViewById(R.id.preferences_ab_perf_mode);
         SwitchCompat updateRecovery = view.findViewById(R.id.preferences_update_recovery);
+        SwitchCompat betaChannel = view.findViewById(R.id.preferences_beta_channel);
 
         if (!Utils.isABDevice()) {
             abPerfMode.setVisibility(View.GONE);
@@ -517,6 +518,7 @@ public class UpdatesActivity extends UpdatesListActivity {
         meteredNetworkWarning.setChecked(prefs.getBoolean(Constants.PREF_METERED_NETWORK_WARNING,
                 prefs.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true)));
         abPerfMode.setChecked(prefs.getBoolean(Constants.PREF_AB_PERF_MODE, false));
+        betaChannel.setChecked(prefs.getBoolean(Constants.PREF_BETA_CHANNEL, false));
 
         if (getResources().getBoolean(R.bool.config_hideRecoveryUpdate)) {
             // Hide the update feature if explicitly requested.
@@ -557,6 +559,7 @@ public class UpdatesActivity extends UpdatesListActivity {
                             .putBoolean(Constants.PREF_METERED_NETWORK_WARNING,
                                     meteredNetworkWarning.isChecked())
                             .putBoolean(Constants.PREF_AB_PERF_MODE, abPerfMode.isChecked())
+                            .putBoolean(Constants.PREF_BETA_CHANNEL, betaChannel.isChecked())
                             .apply();
 
                     if (Utils.isUpdateCheckEnabled(this)) {
--- app/src/main/java/org/lineageos/updater/misc/Utils.java
+++ app/src/main/java/org/lineageos/updater/misc/Utils.java
@@ -159,7 +159,12 @@ public class Utils {
 
         String serverUrl = SystemProperties.get(Constants.PROP_UPDATER_URI);
         if (serverUrl.trim().isEmpty()) {
-            serverUrl = context.getString(R.string.updater_server_url);
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+            if (prefs.getBoolean(Constants.PREF_BETA_CHANNEL, false)) {
+                serverUrl = context.getString(R.string.updater_server_url_beta);
+            } else {
+                serverUrl = context.getString(R.string.updater_server_url);
+            }
         }
 
         return serverUrl.replace("{device}", device)
